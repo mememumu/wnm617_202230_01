d51c754c34d7cc853a078d308bd4329dfec91288
Kristy Wong <kwongcw@Kristys-MacBook-Pro.local>
2022-05-11 00:41:54 -0700

    First commit

---
+++ js/app.js
@@ -0,0 +1,55 @@
+
+$(() => {
+   checkUserId();
+
+   // EVENT DELEGATION
+   $(document)
+
+   // FORM SUBMISSIONS
+   .on("submit", "#signin-form", function(e) {
+      e.preventDefault();
+      checkLoginForm();
+   })
+
+
+   // CLICKS
+   .on("click", ".js-logout", function() {
+      sessionStorage.removeItem("userId");
+      checkUserId();
+   })
+
+
+   // ACTIVATE TOOLS
+   .on("click", "[data-activate]", function() {
+      let target = $(this).data("activate");
+      $(target).addClass("active")
+   })
+   .on("click", "[data-deactivate]", function() {
+      let target = $(this).data("deactivate");
+      $(target).removeClass("active")
+   })
+   .on("click", "[data-toggle]", function() {
+      let target = $(this).data("toggle");
+      $(target).toggleClass("active")
+   })
+   .on("click", "[data-activateone]", function() {
+      let target = $(this).data("activateone");
+      $(target).addClass("active")
+         .siblings().removeClass("active")
+   })
+
+   ;
+
+   $("[data-template]").each(function(){
+      let target = $(this).data("template");
+      $(this).html($(target).html())
+   });
+
+   [,"#recent-page","#list-page","#user-profile-page"].forEach((p,i)=>{
+      if(window.location.hash === p) {
+         console.log($(".nav-icon-set li"))
+         $(`.nav-icon-set li:nth-child(${i})`).addClass("active");
+      }
+   });
+
+});
